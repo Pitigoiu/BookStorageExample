70f8b0eda12b247e762cf114182a288eecc9ddd7
Pitigoiu <denis.pitigoi@yahoo.com>
2023-01-30 18:31:34 +0200

    First send

---
+++ src/contexts/BookContext.js
@@ -0,0 +1,28 @@
+import React,{createContext, useState,useEffect} from 'react';
+import {v4 as uuidv4} from 'uuid';
+
+export const BookContext=createContext();
+
+const BookContextProvider = (props) => {
+    const [books, setBooks] = useState(()=>{
+        const localData=localStorage.getItem('books');
+        return localData ? JSON.parse(localData) : [];
+    });
+      
+    const addBook=(title,author)=>{
+        setBooks([...books,{title:title,author:author,id:uuidv4()}]);
+    }
+    const removeBook=(id)=>{
+        setBooks(books.filter(book=>book.id!==id));
+    }
+    useEffect(()=>{
+        localStorage.setItem('books',JSON.stringify(books));
+    },[books]);
+    return (
+        <BookContext.Provider value={{books,addBook,removeBook}}>
+            {props.children}
+        </BookContext.Provider>
+      );
+}
+ 
+export default BookContextProvider;
---
+++ src/reducers/BookReducer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+export const bookReducer=(state,action)=>{
+    switch(action.type){
+        case 'ADD_BOOK':
+            return []
+    }
+}
